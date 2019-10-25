55f2d12da04ac7b754531cc74167cab56a9faa33
houssemalimi <houssem.alimi@esprit.tn>
2019-10-25 15:12:15 +0200

    template

--- Tech_Event/src/main/java/Test/Test.java
+++ Tech_Event/src/main/java/Test/Test.java
@@ -1,9 +1,18 @@
 
 package Test;
 
-import Entite.Evennement;
-import Service.EvennementService;
+import Entite.Categorie;
+import Entite.Commentaire;
+import Entite.Sujet;
+import Service.CommentaireService;
+import Service.SujetService;
 import java.sql.SQLException;
+import java.util.List;
+
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 
 public class Test {
@@ -11,22 +20,38 @@ public class Test {
 
     
     public static void main(String[] args) throws SQLException {
-      
-           
-            EvennementService us = new EvennementService();
-        //us.creerEvennement(new Evennement("test1","test1","test1","test1","test1"));
-      Evennement u1 = new Evennement(2,"test10000","test1","test1","test1","test1");
-       //us.creerEvennement(u1);
-      //us.supprimerEvennement(u1);
-       
-   //    us.modifierEvennement(u1);
-       boolean a = us.rechercherparNom("test1");
-        System.out.println(a);
-        System.out.println(us.readAll());
-     //  us.creerProduit(u1);
-       
-       
-
+  /*  Commentaire c3=new Commentaire(5,7,77, "sss" ,"bb");
+    CommentaireService service=new  CommentaireService();
+   // service.ajouterCommentaire(c3);
+   // service.ajouterCommentaire(c2);
+    //System.out.println(service.readAll());
+   // service.supprimerCommentaire(c1);
+    service.supprimerCommentaire(c3);
+   // service.modifierCommentaire(c3);
+    //service.modifierCommentaire(c1);
+   Sujet s1=new Sujet(2,1,"mybesttitre", "voir here " ,"madesa3a");
+    SujetService servicee=new  SujetService();
+   //servicee.ajouterSujet(s1);
+   // System.out.println(servicee.readAllS());
+    //servicee.supprimerSujet(s1);
+   // servicee.modifierSujet(s1);
+    boolean a = servicee.rechercherparNom("mybeste");
     }
+    
+   */
+ 
+  SujetService sujet = new SujetService();
+  /* Sujet sujet1 = new Sujet(2, "dtfyj", "jvhhj", Categorie.Astronomie);
+   sujet.ajouterSujet(sujet1);
+        System.out.println(sujet1);
+    */
+ CommentaireService commentaireService = new CommentaireService();
+        System.out.println(commentaireService.readAll(93));
+    }
+    
+  
+   
+
+    
     
 }
